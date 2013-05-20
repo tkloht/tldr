3c919a18fae35fbfc1dc4297dbe32b7eb812515e
Tobias Kloht <tobias.kloht@gmail.com>
2013-05-20 13:44:59 +0200

    Revert "Inital"
    This reverts commit 969f89235bd10f982d689dec6e7a7d3b5b9ddec6.

--- Projects/TLDR-Backend/gcm/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from gcm.models import Device
-
-
-class DeviceForm(forms.ModelForm):
-    class Meta:
-        model = Device
-        fields = ('dev_id', 'reg_id', 'is_active')
