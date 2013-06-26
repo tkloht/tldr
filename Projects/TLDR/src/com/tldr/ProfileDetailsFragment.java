00290779fe9a1cd18327661506dc13114364ba43
ronjadeisel <ronja.deisel@web.de>
2013-06-26 18:42:43 +0200

    User and Task Details selectable from Map UserDetails View improved

--- Projects/TLDR/src/com/tldr/ProfileDetailsFragment.java
+++ Projects/TLDR/src/com/tldr/ProfileDetailsFragment.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView.FindListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -25,13 +27,27 @@ import com.auth.AccountHelper;
 import com.datastore.BaseDatastore;
 import com.datastore.DatastoreResultHandler;
 import com.datastore.TaskDatastore;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.tldr.taskendpoint.model.Task;
 import com.tldr.tools.ToolBox;
 
 public class ProfileDetailsFragment extends Fragment{
+	private MapView mMapView;
+	private GoogleMap mMap;
+	private Bundle mBundle;
 	private double geo_lat;
 	private double geo_lon;
 	HashMap<String, String> hashMap;
+	private Marker userMarker;
+
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -42,6 +58,15 @@ public class ProfileDetailsFragment extends Fragment{
 		this.hashMap = (HashMap<String, String>) bundle.getSerializable("HashMap");
 		this.geo_lat = bundle.getDouble("geo_lat");
 		this.geo_lon = bundle.getDouble("geo_lon");
+		
+		try {
+			MapsInitializer.initialize(getActivity());
+		} catch (GooglePlayServicesNotAvailableException e) {
+			Log.w("TLDR", e);
+		}
+		mMapView = (MapView) v.findViewById(R.id.details_map);
+		mMapView.onCreate(mBundle);
+		setUpMapIfNeeded(v);
 		
 		return v;
 	}
@@ -55,6 +80,47 @@ public class ProfileDetailsFragment extends Fragment{
         TextView user_distance = (TextView) getView().findViewById(R.id.user_distance);
         user_name.setText(hashMap.get("name"));
         user_distance.setText(hashMap.get("distance"));
+        
+        InputMethodManager imm = (InputMethodManager) view.getContext()
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+		mMapView.requestFocus();
 	}
 
+	private void setUpMapIfNeeded(View inflatedView) {
+		if (mMap == null) {
+			mMap = ((MapView) inflatedView.findViewById(R.id.details_map)).getMap();
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
+	
+	
+	private void setUpMap() {
+		mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
+		mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(geo_lat, geo_lon)));
+		this.userMarker = mMap.addMarker(new MarkerOptions().position(
+					new LatLng(geo_lat, geo_lon))
+					.icon(BitmapDescriptorFactory.fromResource(R.drawable.agent)));
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		mMapView.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		mMapView.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		mMapView.onDestroy();
+		super.onDestroy();
+	}
+	
 }
