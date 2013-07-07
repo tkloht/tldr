9060c9881b12ed84cb9c9d50064fdcd211e4ed07
ronjadeisel <ronja.deisel@web.de>
2013-07-07 17:54:48 +0200

    schönere Profile Details: Karte unbeweglich, mit Newsfeed

--- Projects/TLDR/src/com/tldr/ProfileDetailsFragment.java
+++ Projects/TLDR/src/com/tldr/ProfileDetailsFragment.java
@@ -2,7 +2,12 @@ package com.tldr;
 
 
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.Fragment;
@@ -37,6 +42,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.tldr.com.tldr.userinfoendpoint.model.UserInfo;
 import com.tldr.gamelogic.Factions;
 import com.tldr.taskendpoint.model.Task;
 import com.tldr.tools.ToolBox;
@@ -85,6 +91,68 @@ public class ProfileDetailsFragment extends Fragment{
         
         factionlogo = (ImageView) getActivity().findViewById(R.id.fractionPicture);
         Factions.setProfileFractionLogo(factionlogo);
+
+        ListView newsListView=(ListView) view.findViewById(R.id.list_user_news);
+
+        UserInfo u = null;
+        for (UserInfo user: GlobalData.getAllUsers()){
+        	String s = hashMap.get("name");
+        	if (user.getUsername().equals(s)){
+        		u = user;
+        	}
+        }
+        
+		List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+		if (u != null && (u.getAcceptedTasks() != null || ((GlobalData.getCompletedTasks(u) != null) && !GlobalData.getCompletedTasks(u).isEmpty()))){
+			
+			if (u.getAcceptedTasks() != null){
+				for (Long l: u.getAcceptedTasks()){
+					Task t = GlobalData.getTaskById(l);
+					Long timeLong = System.currentTimeMillis();
+					String time = Long.toString(timeLong);
+					Date date = new Date(timeLong);
+					DateFormat formatter = new SimpleDateFormat("d.M. H:mm:ss");
+					String dateFormatted = formatter.format(date);
+					HashMap<String, String> newMap= new HashMap<String, String>();
+					newMap.put("user_name", u.getUsername());
+					newMap.put("action", " accepted ");
+					newMap.put("task_name", t.getTitle());
+					newMap.put("ts", time);
+					newMap.put("date", dateFormatted+ ": ");
+					ToolBox.addInTimeOrder(list, newMap);
+				}
+			}
+			List<Task> completedTasks = GlobalData.getCompletedTasks(u);
+			if (completedTasks != null){
+				for (Task t: completedTasks){
+					Long timeLong = System.currentTimeMillis();
+					String time = Long.toString(timeLong);
+					Date date = new Date(timeLong);
+					DateFormat formatter = new SimpleDateFormat("d.M. H:mm:ss");
+					String dateFormatted = formatter.format(date);
+					HashMap<String, String> newMap= new HashMap<String, String>();
+					newMap.put("user_name", u.getUsername());
+					newMap.put("action", " completed ");
+					newMap.put("task_name", t.getTitle());
+					newMap.put("ts", time);
+					newMap.put("date", dateFormatted+ ": ");
+					ToolBox.addInTimeOrder(list, newMap);
+				}
+			}
+		
+        ListAdapter adapter = new SimpleAdapter(
+        		getActivity(), list,
+                R.layout.layout_news_listitem, new String[] { "date", "user_name", "action", "task_name" },
+                new int[] { R.id.date, R.id.user_name, R.id.action, R.id.task_name});
+        // updating listview
+        newsListView.setAdapter(adapter);
+		}
+		else{
+			view.findViewById(R.id.userNewsfeed).setVisibility(View.GONE);
+		}
+        
+        
+        
         
         InputMethodManager imm = (InputMethodManager) view.getContext()
 				.getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -105,6 +173,11 @@ public class ProfileDetailsFragment extends Fragment{
 	private void setUpMap() {
 		mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
 		mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(geo_lat, geo_lon)));
+		mMap.setIndoorEnabled(false);
+		mMap.setTrafficEnabled(false);
+		mMap.getUiSettings().setAllGesturesEnabled(false);
+		mMap.getUiSettings().setCompassEnabled(false);
+		mMap.getUiSettings().setZoomControlsEnabled(false);
 		this.userMarker = mMap.addMarker(new MarkerOptions().position(
 					new LatLng(geo_lat, geo_lon))
 					.icon(BitmapDescriptorFactory.fromResource(R.drawable.agent)));
